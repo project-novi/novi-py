0434ca13a1f856a344c917bb4cccdac4d7307ea2
mivik <mivikq@gmail.com>
2024-05-28 20:50:45 +0800

    feat(plugin): support plugin dirs and config loading

--- src/novi/file.py
+++ src/novi/file.py
@@ -13,4 +13,4 @@ def set_storage_path(path: Path) -> None:
 
 def object_path(id: Union[UUID, str], variant: str = 'original') -> Path:
     name = str(id) if variant == 'original' else f'{id}.{variant}'
-    return storage_path / name
+    return _storage_path / name
--- src/novi/plugin.py
+++ src/novi/plugin.py
@@ -1,10 +1,19 @@
 import grpc
+import shelve
+import shutil
+import yaml
 
 from datetime import datetime, timezone
+from pathlib import Path
+from pydantic import BaseModel
 
 from novi import Client, Identity, HookPoint
 
+from typing import Optional, Type, TypeVar
+
 _min_utc = datetime.min.replace(tzinfo=timezone.utc)
+
+T = TypeVar('T', bound=BaseModel)
 
 
 class _State:
@@ -12,6 +21,8 @@ class _State:
 
     client: Client
     identity: Identity
+
+    plugin_dir: Optional[Path] = None
 
     def __init__(self):
         self.initialized = False
@@ -24,10 +35,62 @@ class _State:
 _state = _State()
 
 
-def initialize(server: str, identity: str):
+def initialize(
+    server: str,
+    identity: str,
+    plugin_dir: Path,
+    config_template: Optional[Path],
+):
     _state.client = Client(grpc.insecure_channel(server))
     _state.identity = Identity(identity)
+
+    _state.plugin_dir = plugin_dir
+
     _state.initialized = True
+
+    if config_template is not None:
+        config_file = get_config_file()
+        if not config_file.exists():
+            shutil.copy(config_template, config_file)
+
+
+def get_plugin_dir() -> Path:
+    _state.ensure_init()
+    return _state.plugin_dir
+
+
+def get_data_dir() -> Path:
+    path = get_plugin_dir() / 'data'
+    path.mkdir(parents=True, exist_ok=True)
+    return path
+
+
+def get_cache_dir() -> Path:
+    path = get_plugin_dir() / 'cache'
+    path.mkdir(parents=True, exist_ok=True)
+    return path
+
+
+def get_db_dir() -> Path:
+    path = get_plugin_dir() / 'db'
+    path.mkdir(parents=True, exist_ok=True)
+    return path
+
+
+def get_db(name: str) -> shelve.Shelf:
+    from dbm import gnu
+
+    db = gnu.open(str(get_db_dir() / f'{name}.db'))
+    return shelve.Shelf(db)
+
+
+def get_config_file() -> Path:
+    return get_plugin_dir() / 'config.yaml'
+
+
+def load_config(model: Type[T]) -> T:
+    with get_config_file().open() as file:
+        return model.model_validate(yaml.safe_load(file))
 
 
 def get_client() -> Client:
